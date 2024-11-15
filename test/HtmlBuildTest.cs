efbbc16e278c5bd9015a996f7b9c7c7e5d4d7c72
Portella (Ewerton da Silva) <ewerton.portella@200dev.com>
2024-11-15 11:27:53 -0300

    considera o teste com quebra de linha

--- test/HtmlBuildTest.cs
+++ test/HtmlBuildTest.cs
@@ -7,6 +7,7 @@ public class HtmlBuildTest
     [Theory]
     [InlineData("", "<html></html>")]
     [InlineData("a", "<html>a</html>")]
+    [InlineData("a\nB\nc\nD\n", "<html>a\nB\nc\nD\n</html>")]
     public async Task EmptyContentSuccess(string informaed, string expected)
     {
         var arrange = new HtmlBuildRequest
