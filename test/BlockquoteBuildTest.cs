1f4958200383a8b45c5310264e1e688fa956974a
Ewerton da Silva Portella <portella.ewerton@gmail.com>
2024-12-02 23:46:09 +0000

    melhora cenários do bloquote

--- test/BlockquoteBuildTest.cs
+++ test/BlockquoteBuildTest.cs
@@ -5,14 +5,29 @@ public class BlockquoteBuildBuildTest
     [Theory]
     [InlineData(default, default)]
     [InlineData("", "")]
-    [InlineData(">[!NOTE]", "<blockquote><p>[!NOTE]</p></blockquote>")]
-    [InlineData("> [!TIP]", "<blockquote><p>[!TIP]</p></blockquote>")]
-    [InlineData(">  [!IMPORTANT]", "<blockquote><p>[!IMPORTANT]</p></blockquote>")]
-    [InlineData(">    [!WARNING]", "<blockquote><p>[!WARNING]</p></blockquote>")]
-    [InlineData(">     [!CAUTION]", "<blockquote><p>[!CAUTION]</p></blockquote>")]
-    [InlineData(">paragraph 1\n>paragraph 1.1", "<blockquote><p>paragraph 1</p><p>paragraph 1.1</p></blockquote>")]
-    [InlineData("> paragraph 1 \n>   paragraph 1.1       ", "<blockquote><p>paragraph 1</p><p>paragraph 1.1</p></blockquote>")]
-    [InlineData(">paragraph 1\n>paragraph 1.1\n\n>paragraph 2", "<blockquote><p>paragraph 1</p><p>paragraph 1.1</p></blockquote>\n<blockquote><p>paragraph 2</p></blockquote>")]
+    [InlineData(">[!NOTE]", "<blockquote>[!NOTE]</blockquote>")]
+    [InlineData("> [!TIP]", "<blockquote>[!TIP]</blockquote>")]
+    [InlineData(">  [!IMPORTANT]", "<blockquote>[!IMPORTANT]</blockquote>")]
+    [InlineData(">    [!WARNING]", "<blockquote>[!WARNING]</blockquote>")]
+    [InlineData(">     [!CAUTION]", "<blockquote>[!CAUTION]</blockquote>")]
+    [InlineData(
+@">paragraph 1
+>paragraph 1.1", 
+@"<blockquote>paragraph 1
+paragraph 1.1</blockquote>")]
+    [InlineData(
+@"> paragraph 1 
+>   paragraph 1.1       ", 
+@"<blockquote>paragraph 1 
+paragraph 1.1       </blockquote>")]
+    [InlineData(
+@">paragraph 1
+>paragraph 1.1
+
+>paragraph 2", 
+@"<blockquote>paragraph 1
+paragraph 1.1</blockquote>
+<blockquote>paragraph 2</blockquote>")]
     [InlineData(@"
 [Gpt](https://chatgpt.com/) Descreva pra mim por gentileza a definição e uso de feature foogles sem qualquer detalhe técnico ou de sowftware por gentileza?
 
