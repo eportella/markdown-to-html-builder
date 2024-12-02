002e64e133ae89a21fcfe807a45c857890dcb472
Ewerton da Silva Portella <portella.ewerton@gmail.com>
2024-12-02 17:12:36 +0000

    melhora blockquote

--- test/BlockquoteBuildTest.cs
+++ test/BlockquoteBuildTest.cs
@@ -13,6 +13,22 @@ public class BlockquoteBuildBuildTest
     [InlineData(">paragraph 1\n>paragraph 1.1", "<blockquote><p>paragraph 1</p><p>paragraph 1.1</p></blockquote>")]
     [InlineData("> paragraph 1 \n>   paragraph 1.1       ", "<blockquote><p>paragraph 1</p><p>paragraph 1.1</p></blockquote>")]
     [InlineData(">paragraph 1\n>paragraph 1.1\n\n>paragraph 2", "<blockquote><p>paragraph 1</p><p>paragraph 1.1</p></blockquote>\n<blockquote><p>paragraph 2</p></blockquote>")]
+    [InlineData(@"
+[Gpt](https://chatgpt.com/) Descreva pra mim por gentileza a definição e uso de feature foogles sem qualquer detalhe técnico ou de sowftware por gentileza?
+
+># Feature Toggles
+>
+>*Feature toggles*, ou *feature flags*, são uma maneira de controlar a exibição de funcionalidades em um sistema. Elas permitem que você ative ou desative recursos sem precisar fazer mudanças complexas.
+>
+>Por exemplo, se você tem uma nova funcionalidade que ainda não quer que todos vejam, pode mantê-la oculta até que esteja pronta.
+>
+>## Vantagens
+>
+>- **Testes**: Útil para testar novidades com um grupo menor de pessoas antes de lançar para todos.
+>- **Ajustes**: Facilita ajustes com base no feedback.
+>- **Gerenciamento**: Ajuda a gerenciar como e quando as novas funcionalidades são apresentadas aos usuários.
+>
+>No geral, os *feature toggles* aumentam a agilidade e a segurança nas implementações.", "")]
     public async Task Success(string informed, string expected)
     {
         var arrange = new BlockquoteBuildRequest
