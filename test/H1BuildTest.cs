359babf2bd0c5c542967c42fc3563f82c71de41d
Ewerton da Silva Portella <portella.ewerton@gmail.com>
2024-12-02 18:45:52 +0000

    trata demais Hn

--- test/H1BuildTest.cs
+++ test/H1BuildTest.cs
@@ -7,6 +7,19 @@ public class H1BuildTest
     [InlineData("# #", "<h1>#</h1>")]
     [InlineData("# #Headding", "<h1>#Headding</h1>")]
     [InlineData("# # Headding", "<h1># Headding</h1>")]
+    [InlineData(@"
+# tilte
+## tilte
+### tilte
+#### tilte
+##### tilte
+###### tilte", @"
+<h1>tilte</h1>
+## tilte
+### tilte
+#### tilte
+##### tilte
+###### tilte")]
     public async Task Success(string informed, string expected)
     {
         var arrange = new HtmlH1StringBuildRequest
