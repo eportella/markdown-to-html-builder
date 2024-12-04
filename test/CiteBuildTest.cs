ab447a3cdbb69f7af50bc503b92d88e9132c5a50
Ewerton da Silva Portella <portella.ewerton@gmail.com>
2024-12-04 16:14:49 +0000

    trata os starts no CITE

--- test/CiteBuildTest.cs
+++ test/CiteBuildTest.cs
@@ -4,8 +4,8 @@ public class CiteABuildTest
 {
     [Theory]
     [InlineData(@"[^1]: Meu primeiro contato.", @"<br/><cite id=""cite-1""><a href=""#cited-1"">(1)</a>. Meu primeiro contato.</cite>")]
-    [InlineData(@"<p>[^2]: Meu segundo contato.", @"<br/><cite id=""cite-2""><a href=""#cited-2"">(2)</a>. Meu segundo contato.</cite>")]
-    [InlineData(@"</h>[^3]: Meu terceiro contato.", @"<br/><cite id=""cite-3""><a href=""#cited-3"">(3)</a>. Meu terceiro contato.</cite>")]
+    [InlineData(@"<p>[^2]: Meu segundo contato.", @"<p><br/><cite id=""cite-2""><a href=""#cited-2"">(2)</a>. Meu segundo contato.</cite>")]
+    [InlineData(@"</h>[^3]: Meu terceiro contato.", @"</h><br/><cite id=""cite-3""><a href=""#cited-3"">(3)</a>. Meu terceiro contato.</cite>")]
     public async Task Success(string informed, string expected)
     {
         var arrange = new HtmlCiteStringBuildRequest
