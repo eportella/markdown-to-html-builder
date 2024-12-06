789c8604e22328a5614dbb17101ac9bfb64da147
Ewerton da Silva Portella <portella.ewerton@gmail.com>
2024-12-06 14:02:10 +0000

    exclui o blockquoteFormat

--- console/Mediatr.cs
+++ console/Mediatr.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using HtmlAgilityPack;
@@ -88,118 +87,6 @@ internal sealed class MarkdownFileInfoGetStreamHandler : IStreamRequestHandler<M
             yield return item;
 
         await Task.Yield();
-    }
-}
-
-internal sealed class BlockquoteFormatRequest : IRequest<string?>
-{
-    public FileInfo? FileInfo { get; init; }
-}
-internal sealed class BlockquoteFormatRequestHandler : IRequestHandler<BlockquoteFormatRequest, string?>
-{
-    public async Task<string?> Handle(BlockquoteFormatRequest request, CancellationToken cancellationToken)
-    {
-        using var stream = request.FileInfo!.OpenRead();
-        await Task.Yield();
-        var htmlDocument = new HtmlDocument();
-        htmlDocument.Load(stream);
-
-        var blockquotes = htmlDocument.DocumentNode.SelectNodes("//blockquote");
-
-        if (blockquotes == null)
-            return htmlDocument.DocumentNode.OuterHtml;
-
-        foreach (var blockquote in blockquotes)
-        {
-            var p = blockquote.SelectSingleNode("p");
-            if (p == null)
-                continue;
-
-            if (p.InnerText.Trim().StartsWith("[!NOTE]"))
-            {
-                var highlight = new Highlight
-                {
-                    Key = "[!NOTE]",
-                    Color = "#1f6feb",
-                    Name = "Note"
-                };
-                blockquote.SetAttributeValue("style", $"border-color: {highlight.Color};");
-                Format(p, highlight);
-                continue;
-            }
-            if (p.InnerText.Trim().StartsWith("[!TIP]"))
-            {
-                var highlight = new Highlight
-                {
-                    Key = "[!TIP]",
-                    Color = "#3fb950",
-                    Name = "Tip"
-                };
-                blockquote.SetAttributeValue("style", $"border-color: {highlight.Color};");
-                Format(p, highlight);
-                continue;
-            }
-            if (p.InnerText.Trim().StartsWith("[!IMPORTANT]"))
-            {
-                var highlight = new Highlight
-                {
-                    Key = "[!IMPORTANT]",
-                    Color = "#ab7df8",
-                    Name = "Important"
-                };
-                blockquote.SetAttributeValue("style", $"border-color: {highlight.Color};");
-                Format(p, highlight);
-                continue;
-            }
-            if (p.InnerText.Trim().StartsWith("[!WARNING]"))
-            {
-                var highlight = new Highlight
-                {
-                    Key = "[!WARNING]",
-                    Color = "#d29922",
-                    Name = "Warning"
-                };
-                blockquote.SetAttributeValue("style", $"border-color: {highlight.Color};");
-                Format(p, highlight);
-                continue;
-            }
-            if (p.InnerText.Trim().StartsWith("[!CAUTION]"))
-            {
-                var highlight = new Highlight
-                {
-                    Key = "[!CAUTION]",
-                    Color = "#f85149",
-                    Name = "Caution"
-                };
-                blockquote.SetAttributeValue("style", $"border-color: {highlight.Color};");
-                Format(p, highlight);
-                continue;
-            }
-        }
-
-        return htmlDocument.DocumentNode.OuterHtml;
-    }
-
-    private static void Format(HtmlNode p, Highlight highlight)
-    {
-        p.SetAttributeValue("style", "display:flex; align-items:center; column-gap:0.4em; font-weight:500;");
-
-        if (highlight.Key != default)
-            p.InnerHtml = p.InnerHtml.Replace(highlight.Key, string.Empty);
-
-        if (highlight.Color != default)
-        {
-            var span = HtmlNode.CreateNode($"<span style='color:{highlight.Color};'>{highlight.Name}</span>");
-            p.PrependChild(span);
-            p.PrependChild(HtmlNode.CreateNode(highlight.Key));
-        }
-    }
-
-    public struct Highlight
-    {
-        public string? Key { get; set; }
-        public string? Color { get; set; }
-        public string? Name { get; set; }
     }
 }
 
