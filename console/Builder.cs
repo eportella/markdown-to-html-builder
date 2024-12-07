4bca41f67cf897d90862cc5b35ddbaf292f7a2d3
Portella (Notebook) <portella@tradetech.com.br>
2024-12-07 15:19:05 -0300

    implementa cite

--- console/Builder.cs
+++ console/Builder.cs
@@ -114,17 +114,17 @@ internal sealed class MarkdownFileInfoBuildRequesttHandler(IMediator mediator) :
         var content = (
             await mediator
                 .Send(new BuildRequest
-                    {
-                        Title = Environment.GetCommandLineArgs()[4],
-                        Url = Environment.GetCommandLineArgs()[5],
-                        Source = await mediator
-                            .Send(new FileInfoTextReadRequest 
-                                { 
-                                    FileInfo = request.Source 
-                                }, 
+                {
+                    Title = Environment.GetCommandLineArgs()[4],
+                    Url = Environment.GetCommandLineArgs()[5],
+                    Source = await mediator
+                            .Send(new FileInfoTextReadRequest
+                            {
+                                FileInfo = request.Source
+                            },
                                 cancellationToken
                             ),
-                    }, 
+                },
                     cancellationToken
                 )
             )?
@@ -251,6 +251,14 @@ internal class LI : IElement
     public IElement? Parent { get; init; }
     public IElement[]? Children { get; internal set; }
     public string? Built { get => $"<li>{Children.Build()}</li>"; }
+    internal string? Source { get; init; }
+}
+
+internal class Cite : IElement
+{
+    public IElement? Parent { get; init; }
+    public IElement[]? Children { get; internal set; }
+    public string? Built { get; internal set; }
     internal string? Source { get; init; }
 }
 
@@ -464,6 +472,15 @@ internal sealed class BuildRequestHandler : IRequestHandler<BuildRequest, BuildR
             yield return element;
     }
 
+    private static IEnumerable<IElement> Build(Cite? parent, string? source)
+    {
+        if (source == default)
+            yield break;
+
+        foreach (IElement element in Build(parent, Regex.Matches(source, @$"({TEXT})", RegexOptions.Multiline)))
+            yield return element;
+    }
+
     private static string? Build(string? source)
     {
         if (source == default)
@@ -533,13 +550,6 @@ internal sealed class BuildRequestHandler : IRequestHandler<BuildRequest, BuildR
                 "#f85149",
                 "M4.47.22A.749.749 0 0 1 5 0h6c.199 0 .389.079.53.22l4.25 4.25c.141.14.22.331.22.53v6a.749.749 0 0 1-.22.53l-4.25 4.25A.749.749 0 0 1 11 16H5a.749.749 0 0 1-.53-.22L.22 11.53A.749.749 0 0 1 0 11V5c0-.199.079-.389.22-.53Zm.84 1.28L1.5 5.31v5.38l3.81 3.81h5.38l3.81-3.81V5.31L10.69 1.5ZM8 4a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 8 4Zm0 8a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"
             );
-        }, RegexOptions.Multiline);
-
-        target = Regex.Replace(target, @$"({CITE})", (match) =>
-        {
-            var index = match.Groups["CITE_INDEX"].Value;
-            var content = match.Groups["CITE_CONTENT"].Value;
-            return @$"<cite id=""cite-{index}""><a href=""#cited-{index}"">({index})</a>. {content}</cite>";
         }, RegexOptions.Multiline);
 
         target = Regex.Replace(target, @$"({CITED})", (match) =>
@@ -726,6 +736,22 @@ internal sealed class BuildRequestHandler : IRequestHandler<BuildRequest, BuildR
                     };
                     p.Children = Build(p, content).ToArray();
                     yield return p;
+                    continue;
+                }
+            }
+            {
+                var index = match.Groups["CITE_INDEX"].Value;
+                var content = match.Groups["CITE_CONTENT"].Value;
+                if (!string.IsNullOrWhiteSpace(index))
+                {
+                    var cite = new Cite
+                    {
+                        Source = content,
+                        Parent = parent,
+                    };
+                    cite.Built = @$"<cite id=""cite-{index}""><a href=""#cited-{index}"">({index})</a>. {cite.Children.Build()}</cite>";
+                    cite.Children = Build(cite, content).ToArray();
+                    yield return cite;
                     continue;
                 }
             }
