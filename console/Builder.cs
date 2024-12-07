f7f522e6eb712b43fbf866e062883c24882ee3d4
Portella (Notebook) <portella@tradetech.com.br>
2024-12-07 16:11:18 -0300

    trata p como no github para considerar um novo com um enter extra

--- console/Builder.cs
+++ console/Builder.cs
@@ -296,7 +296,7 @@ internal static class IElementExtensions
 
 internal sealed class BuildRequestHandler : IRequestHandler<BuildRequest, BuildResponse>
 {
-    const string P = @"^(?'P'(?!(#|>| *-| *\d+\.|\[\^\d+\]:)).+(\r?\n|)*)";
+    const string P = @"^(?'P'((?!(#|>| *-| *\d+\.|\[\^\d+\]:)).+(\r?\n|))+(\r?\n|))";
     const string H1 = @"^(?'H1'# *(?'H1_CONTENT'(?!#).+(\r?\n|)))";
     const string H2 = @"^(?'H2'## *(?'H2_CONTENT'(?!#).+(\r?\n|)))";
     const string H3 = @"^(?'H3'### *(?'H3_CONTENT'(?!#).+(\r?\n|)))";
