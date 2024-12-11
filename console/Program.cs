202b5978f9451dbcbbc7426401cf9ad584fc986e
Ewerton da Silva Portella <portella.ewerton@gmail.com>
2024-12-11 09:27:29 -0300

    configura a criação do InputBuildResponse

--- console/Program.cs
+++ console/Program.cs
@@ -6,6 +6,16 @@ await new ServiceCollection()
     .AddTransient(typeof(IPipelineBehavior<,>), typeof(TimeElapsedPipelineBehavior<,>))
     .AddTransient(typeof(IStreamPipelineBehavior<,>), typeof(TimeElapsedStreamPipelineBehavior<,>))
     .AddMediatR(mediatorServiceConfiguration => mediatorServiceConfiguration.RegisterServicesFromAssemblyContaining<MarkdownToHtmlBuildRequest>())
+    .AddSingleton<InputBuildResponse>((serviceProvider)=>{
+        return new InputBuildResponse
+        {
+            SourcePath = default,
+            TargetPath = default,
+            TargetFileName = default,
+            RepositoryOnwer = default,
+            BaseUrl = default,
+        };
+    })
     .BuildServiceProvider()
     .GetRequiredService<IMediator>()
     .Send(new MarkdownToHtmlBuildRequest
