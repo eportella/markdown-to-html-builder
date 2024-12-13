b228fd2a76d3839dfce2ef310b1fb93e6358fa75
Portella (Notebook) <portella@tradetech.com.br>
2024-12-13 11:19:05 -0300

    corrige cenário do input

--- .test/InputBuildTest.cs
+++ .test/InputBuildTest.cs
@@ -4,7 +4,7 @@ public class InputBuildTest
 {
     [Theory]
     [InlineData(
-@"/home/runner/work/_actions/eportella/markdown-to-html-builder/main/console/bin/Debug/net8.0/console.dll --source-path /home/runner/work/markdown-to-html-builder/markdown-to-html-builder --target-path /home/runner/work/markdown-to-html-builder/markdown-to-html-builder/_site --target-file-name index.html --repository_owner eportella --source-url-base https://eportella.github.io/markdown-to-html-builder")]
+@"/home/runner/work/_actions/eportella/markdown-to-html-builder/main/console/bin/Debug/net8.0/console.dll --source-path /home/runner/work/markdown-to-html-builder/markdown-to-html-builder --target-path /home/runner/work/markdown-to-html-builder/markdown-to-html-builder/_site --target-file-name index.html --repository_owner eportella --source-url-base https://eportella.github.io/markdown-to-html-builder --action-path /home/runner/work/markdown-to-html-builder")]
     public async Task Success(string informed)
     {
         var arrange = new InputBuildRequest
