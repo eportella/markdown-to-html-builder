4f5d2ef38f2cb2359aa50ef1a0c035aad4dc7e05
Portella (Notebook) <portella@tradetech.com.br>
2024-12-20 12:00:06 -0300

    cobre novamente o cenário do a

--- .test/ABuildTest.cs
+++ .test/ABuildTest.cs
@@ -1,5 +1,3 @@
-using MediatR;
-using Moq;
 
 namespace test;
 
@@ -8,30 +6,29 @@ public class ABuildTest
     [Theory]
     [InlineData(
 @"multi link [link1](https://route-1/path/readme.md), [link2](https://route-2/readme.md) e [link3](https://route-3/readme.md) text, contnue text [link4](https://route-4/README.md).",
-@"<p>multi link <a href=""https://route-1/path/readme.md"">link1</a>, <a href=""https://route-2/readme.md"">link2</a> e <a href=""https://route-3/readme.md"">link3</a> text, contnue text <a href=""https://route-4/README.md"">link4</a>.</p>")]
+@"multi link <a href=""https://route-1/path/readme.md"">link1</a>, <a href=""https://route-2/readme.md"">link2</a> e <a href=""https://route-3/readme.md"">link3</a> text, contnue text <a href=""https://route-4/README.md"">link4</a>.")]
     [InlineData(
 @"um [hiperlink[^15]](https://teste/) formatado",
-@"<p>um <a href=""https://teste/"">hiperlink<cite id=""cited-15""><a href=""#cite-15""><sup>(15)</sup></a></cite></a> formatado</p>")]
+@"um <a href=""https://teste/"">hiperlink<cite id=""cited-15""><a href=""#cite-15""><sup>(15)</sup></a></cite></a> formatado")]
     public async Task Success(string informed, string expected)
     {
-        var arrange = new BuildRequest
+        var arrange = new ABuildRequest
         {
             Source = informed
         };
 
-        var result = await new BuildRequestHandler(
+        var result = await new ABuildRequestHandler(
                 new ProjectBuildResponse
                 {
                     Title = "--title--",
                     BaseUrl = new Uri("https://route-1")
-                },
-                 Mock.Of<IMediator>()
+                }
             )
             .Handle(
                 arrange,
                 CancellationToken.None
             );
 
-        Assert.Contains(expected, result.Target?.Built);
+        Assert.Equal(expected, result!.Target);
     }
 }
