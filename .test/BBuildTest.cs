36c4ff0dfb4296cd9613cdc2d4e39edc0878c05f
Portella (Notebook) <portella@tradetech.com.br>
2024-12-20 11:31:47 -0300

    cobre novamente o cenário do b

--- .test/BBuildTest.cs
+++ .test/BBuildTest.cs
@@ -8,44 +8,37 @@ public class BBuildTest
     [Theory]
     [InlineData(
 @"prefix **infix bold** sufix",
-@"<p>prefix <b>infix bold</b> sufix</p>")]
+@"prefix <b>infix bold</b> sufix")]
     [InlineData(
 @"**prefix bold** infix sufix",
-@"<p><b>prefix bold</b> infix sufix</p>")]
+@"<b>prefix bold</b> infix sufix")]
     [InlineData(
 @"prefix infix **sufix bold**",
-@"<p>prefix infix <b>sufix bold</b></p>")]
+@"prefix infix <b>sufix bold</b>")]
     [InlineData(
 @"**text bold**",
-@"<p><b>text bold</b></p>")]
+@"<b>text bold</b>")]
     [InlineData(
 @" **bold** **bold** ",
-@"<p> <b>bold</b> <b>bold</b> </p>")]
+@" <b>bold</b> <b>bold</b> ")]
     [InlineData(
 @"**bold**
 **bold**",
-@"<p><b>bold</b>
-<b>bold</b></p>")]
+@"<b>bold</b>
+<b>bold</b>")]
     public async Task Success(string informed, string expected)
     {
-        var arrange = new BuildRequest
+        var arrange = new BBuildRequest
         {
             Source = informed
         };
 
-        var result = await new BuildRequestHandler(
-                new ProjectBuildResponse
-                {
-                    Title = "--title--",
-                    BaseUrl = new Uri("https://github.com")
-                },
-                Mock.Of<IMediator>()
-            )
+        var result = await new BBuildRequestHandler()
             .Handle(
                 arrange,
                 CancellationToken.None
             );
 
-        Assert.Contains(expected, result.Target?.Built);
+        Assert.Contains(expected, result!.Target);
     }
 }
