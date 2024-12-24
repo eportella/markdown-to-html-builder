3da11d91bba2dc2752bb5b248bfdfcc39d4a785e
Portella (Notebook) <portella@tradetech.com.br>
2024-12-23 22:49:25 -0300

    remove variável

--- .library/HtmlBuild.cs
+++ .library/HtmlBuild.cs
@@ -14,9 +14,7 @@ internal sealed class HtmlBuildRequestHandler(ProjectBuildResponse project, IMed
         if (request.Source == default)
             return default;
 
-        var replaced = await mediator.Send(new BlockBuildRequest { Source = request.Source }, cancellationToken);
-
-        var bodyBuilt = @$"<body><h1><a href=""{project.BaseUrl}""/>{project.Title}</a></h1>{replaced}{(project.OwnerTitle != default && project.OwnerBaseUrl != default ? @$"<span class=""owner""><a href=""{project.OwnerBaseUrl}""/>{project.OwnerTitle}</a></span>" : string.Empty)}</body>";
+        var bodyBuilt = @$"<body><h1><a href=""{project.BaseUrl}""/>{project.Title}</a></h1>{await mediator.Send(new BlockBuildRequest { Source = request.Source }, cancellationToken)}{(project.OwnerTitle != default && project.OwnerBaseUrl != default ? @$"<span class=""owner""><a href=""{project.OwnerBaseUrl}""/>{project.OwnerTitle}</a></span>" : string.Empty)}</body>";
         return $@"<!DOCTYPE html><html lang=""pt-BR""><head><title>{project.Title}</title><meta content=""text/html; charset=UTF-8;"" http-equiv=""Content-Type"" /><meta name=""viewport"" content=""width=device-width, initial-scale=1.0""><meta name=""color-scheme"" content=""dark light""><link rel=""stylesheet"" href=""{project.BaseUrl!.ToString().TrimEnd('/')}/stylesheet.css""></style></head>{bodyBuilt}</html>";
    }
 }
