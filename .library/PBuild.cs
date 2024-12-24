536ce8ad7b16f6b968d58018cf120c6d459c6e16
Portella (Notebook) <portella@tradetech.com.br>
2024-12-24 01:00:11 -0300

    usa o p em tempo de compilação

--- .library/PBuild.cs
+++ .library/PBuild.cs
@@ -4,21 +4,19 @@ internal sealed class PBuildRequest : IRequest<string?>
 {
     internal string? Source { get; init; }
 }
-internal sealed class PBuildRequestHandler(IMediator mediator) : IRequestHandler<PBuildRequest, string?>
+internal sealed partial class PBuildRequestHandler(IMediator mediator) : IRequestHandler<PBuildRequest, string?>
 {
     const string PATTERN = @"^(?'P'((?!(#|>| *-| *\d+\.|\[\^\d+\]:)).+(\r?\n|))+(\r?\n|))";
-    static Regex Regex { get; }
-    static PBuildRequestHandler()
-    {
-        Regex = new Regex(PATTERN, RegexOptions.Multiline);
-    }
+    [GeneratedRegex(PATTERN, RegexOptions.Multiline)]
+    private static partial Regex Regex();
+    
     public async Task<string?> Handle(PBuildRequest request, CancellationToken cancellationToken)
     {
         await Task.Yield();
         if (request.Source == default)
             return default;
 
-        return Regex.Replace(request.Source, match =>
+        return Regex().Replace(request.Source, match =>
         {
             var children = mediator
                 .Send(new InlineBuildRequest { Source = match.Groups["P"].Value }, cancellationToken).Result;
