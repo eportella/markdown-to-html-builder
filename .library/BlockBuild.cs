23aedee12a1fc46cd6f6cec80f3e15e0a9ff3ac4
Portella (Notebook) <portella@tradetech.com.br>
2024-12-23 22:32:03 -0300

    tenta abstrair ulol

--- .library/BlockBuild.cs
+++ .library/BlockBuild.cs
@@ -8,20 +8,11 @@ internal sealed class BlockBuildRequest : IRequest<string?>
 internal sealed class BlockBuildRequestHandler(IMediator mediator) : IRequestHandler<BlockBuildRequest, string?>
 {
     const string P = @"^(?'P'((?!(#|>| *-| *\d+\.|\[\^\d+\]:)).+(\r?\n|))+(\r?\n|))";
-    const string UL_OL = @"^(?'UL_OL'(((?'UL'-)|(?'OL'\d+\.)) *.+(\r?\n|))( *((-)|(\d+\.)) *.+(\r?\n|))*(\r?\n|))";
-    const string UL_OL_INNER = @"^(((.+?\r?\n))(?'UL_OL'( *((-)|(\d+\.)) *.+(\r?\n|))*(\r?\n|)))";
-    const string LI = @"^(-|\d+\.) *(?'LI'(.*(\r?\n|)+(?!(-|\d+\.)))+(\r?\n|))";
     static Regex RegexBlock { get; }
-    static Regex RegexOlUl { get; }
-    static Regex RegexOlUlInner { get; }
-    static Regex RegexLi { get; }
 
     static BlockBuildRequestHandler()
     {
-        RegexBlock = new Regex(@$"({P}|{H1BuildRequestHandler.PATTERN}|{H2BuildRequestHandler.PATTERN}|{H3BuildRequestHandler.PATTERN}|{H4BuildRequestHandler.PATTERN}|{H5BuildRequestHandler.PATTERN}|{H6BuildRequestHandler.PATTERN}|{BlockquoteBuildRequestHandler.PATTERN}|{UL_OL}|{CiteBuildRequestHandler.PATTERN})", RegexOptions.Multiline);
-        RegexOlUl = new Regex(UL_OL);
-        RegexOlUlInner = new Regex(UL_OL_INNER, RegexOptions.Multiline);
-        RegexLi = new Regex(LI, RegexOptions.Multiline);
+        RegexBlock = new Regex(@$"({P}|{H1BuildRequestHandler.PATTERN}|{H2BuildRequestHandler.PATTERN}|{H3BuildRequestHandler.PATTERN}|{H4BuildRequestHandler.PATTERN}|{H5BuildRequestHandler.PATTERN}|{H6BuildRequestHandler.PATTERN}|{BlockquoteBuildRequestHandler.PATTERN}|{UlOlBuildRequestHandler.PATTERN}|{CiteBuildRequestHandler.PATTERN})", RegexOptions.Multiline);
     }
 
     public Task<string?> Handle(BlockBuildRequest request, CancellationToken cancellationToken)
@@ -32,36 +23,6 @@ internal sealed class BlockBuildRequestHandler(IMediator mediator) : IRequestHan
         return Task.FromResult(RegexBlock?.Replace(request.Source, (match) => Replace(match, cancellationToken)));
     }
 
-    private async IAsyncEnumerable<string?> Build(string? source, [EnumeratorCancellation] CancellationToken cancellationToken)
-    {
-        if (source == default)
-            yield break;
-
-        foreach (Group group in RegexOlUlInner
-            .Matches(source)
-            .Select(m => m.Groups["UL_OL"])
-            .Where(g => g.Success && !string.IsNullOrWhiteSpace(g.Value)))
-        {
-            source = source
-                .Replace(
-                    group.Value,
-                    RegexOlUl
-                        .Replace(
-                            Regex
-                                .Replace(
-                                    group.Value,
-                                    "^    ",
-                                    string.Empty,
-                                    RegexOptions.Multiline
-                                ),
-                                match => Replace(match, cancellationToken)
-                        )
-                );
-        }
-
-        await foreach (var text in mediator.CreateStream(new InlineBuildRequest { Source = source }, cancellationToken))
-            yield return text;
-    }
     private string Replace(Match match, CancellationToken cancellationToken)
     {
 
@@ -83,25 +44,7 @@ internal sealed class BlockBuildRequestHandler(IMediator mediator) : IRequestHan
         {
             var content = match.Groups["UL_OL"].Value;
             if (!string.IsNullOrWhiteSpace(content))
-            {
-                if (!string.IsNullOrWhiteSpace(match.Groups["UL"].Value))
-                    return $"<ul>{RegexLi.Replace(content, match => Replace(match, cancellationToken))}</ul>";
-
-                if (!string.IsNullOrWhiteSpace(match.Groups["OL"].Value))
-                    return $"<ol>{RegexLi.Replace(content, match => Replace(match, cancellationToken))}</ol>";
-
-                return content;
-            }
-        }
-
-        {
-            var content = match.Groups["LI"].Value;
-            if (!string.IsNullOrWhiteSpace(content))
-            {
-                var children = Build(content, cancellationToken)
-                    .ToBlockingEnumerable(cancellationToken);
-                return $"<li>{children.Build()}</li>";
-            }
+                return mediator.Send(new UlOlBuildRequest { Source = content }, cancellationToken).Result;
         }
 
         {
